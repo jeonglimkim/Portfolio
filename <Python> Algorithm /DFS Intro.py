6ccd69cb2d04c0ca8364bdf826be78e9d97461d5
jeonglimkim <jkim9536@uchicago.edu>
2022-03-02 23:43:37 +0900

    재귀함수를 이용한 이진수 출력
    10진수 N이 입력되면 2진수로 변환하여 출력하는 프로그램을 작성하세요. 단 재귀함수를 이용해서 출력해야 합니다.
    
    ▣ 입력설명
    첫 번째 줄에 10진수 N(1<=N<=1,000)이 주어집니다.
    
    ▣ 출력설명
    첫 번째 줄에 이진수를 출력하세요.
    
    ▣ 입력예제 1
    11
    
    ▣ 출력예제 1
    1011

--- <Python> Algorithm /DFS Intro.py
+++ <Python> Algorithm /DFS Intro.py
@@ -1,1 +1,12 @@
-def DFS(v):    if v == 0:        return     else:        DFS(v // 2)        print(v % 2, end = "")if __name__ == "__main__":    n = int(input())    DFS(n)        
+
+def DFS(v):
+    if v == 0:
+        return
+    else:
+        DFS(v // 2)
+        print (v % 2, end = "")
+        
+        
+if __name__ == "__main__":
+    n = int(input())
+    DFS(n)
