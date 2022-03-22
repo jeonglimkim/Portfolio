06ee1b4e19d16afad4d2237d15f34e6e193ac2b7
jeonglimkim <jkim9536@uchicago.edu>
2022-03-22 22:29:52 +0900

    숫자의 개수
    백준 문제 2557
    (시간초과)

---
+++ <Python> Algorithm /숫자의개수.py
@@ -0,0 +1,20 @@
+
+from pickletools import markobject
+
+
+if __name__ == "__main__":
+    n = int(input())
+    cnt = 0 
+    temp = n
+    
+    while True:
+        a = temp // 10 #Quotient
+        b = temp % 10  #Remainder
+        c = (a + b) % 10 
+        temp = (b*10) + c
+        cnt += 1 
+        if n == temp:
+            break
+    
+    print(cnt)
+        
