422cd2782a9d79f63e30459d9b47a2dd88feccb7
jeonglimkim <jkim9536@uchicago.edu>
2022-03-24 22:17:24 +0900

    숫자의 개수
    백준 2557번 문제
    Correct answer
    https://www.acmicpc.net/problem/2577

--- <Python> Algorithm /탐색 & 시뮬레이션 /숫자의개수.py
+++ <Python> Algorithm /탐색 & 시뮬레이션 /숫자의개수.py
@@ -3,11 +3,14 @@ if __name__ == "__main__":
     a = int(input())
     b = int(input())
     c = int(input())
-    result = a * b * c
-    cnt = []*10
+    n = a * b * c
+    nums = [0] * 10 
     
-    for i in range (10):
-        if result // 10 == i:
-            cnt[i] += 1
+    while n != 0:
+        r = n % 10
+        nums[r] += 1
+        n = n // 10
     
-    print(cnt)
+    for i in range(10):
+        print(nums[i])
+
