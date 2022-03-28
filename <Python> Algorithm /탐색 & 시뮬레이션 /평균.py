4bfdccfee0102d948d61d5c9c865b94b88faf7b1
jeonglimkim <jkim9536@uchicago.edu>
2022-03-28 11:03:02 +0900

    평균
    백준문제 1546
    https://www.acmicpc.net/problem/1546

---
+++ <Python> Algorithm /탐색 & 시뮬레이션 /평균.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+    n = int(input())
+    score = list(map(int, input().split()))
+    m = max(score)
+    temp = 0
+    
+    for i in range(n):
+        score[i] = score[i] / m * 100
+        temp += score[i]
+        
+    print(temp)
