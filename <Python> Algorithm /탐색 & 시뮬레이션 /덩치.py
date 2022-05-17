b2bbca087dbec4110335bbfeb806a57747c653bb
jeonglimkim <jkim9536@uchicago.edu>
2022-05-17 18:36:07 +0900

    덩치
    백준 10815번
    https://www.acmicpc.net/problem/10815

---
+++ <Python> Algorithm /탐색 & 시뮬레이션 /덩치.py
@@ -0,0 +1,19 @@
+
+if __name__ == "__main__":
+    n = int(input)
+    student_list = []
+
+    for _ in range(n):
+        weight, height = map(int, input().split())
+        student_list.append((weight, height))
+
+    for i in student_list:
+        rank = 1
+        for j in student_list:
+            if i[0] < j[0] and i[1] < j[1]:
+                    rank += 1
+        print(rank, end = " ")
+        
+        
+        weight, height = map(int, input().split())
+    
