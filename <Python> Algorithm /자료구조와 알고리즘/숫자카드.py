183de086031ba2406f867d6d9f06f213df932b17
jeonglimkim <jkim9536@uchicago.edu>
2022-05-20 17:53:29 +0900

    숫자카드
    백준문제 10815번
    https://www.acmicpc.net/problem/10815

---
+++ <Python> Algorithm /자료구조와 알고리즘/숫자카드.py
@@ -0,0 +1,25 @@
+
+if __name__ == "__main__":
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    m = int(input())
+    b = list(map(int, input().split()))
+    
+    for i in range(m):
+        left = 0
+        right = n - 1
+        answer = False
+        while left <= right:
+            mid = (left + right) // 2
+            if a[mid] < b[i]:
+                left = mid + 1
+            elif a[mid] > b[i]:
+                right = mid - 1
+            else:
+                answer = True
+                break
+        if answer == True:
+            print(1, end = ' ')
+        else:
+            print(0, end = ' ')
